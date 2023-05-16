c7f4c50f1ab79bcbe89656533bda4b7cfb60f1e5
Onigiri <kethrinlie@yahoo.com>
2023-05-16 10:01:19 +0300

    Init setup

---
+++ src/main/java/com/challenge/VSPBackend/VspBackendApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.VSPBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VspBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VspBackendApplication.class, args);
+	}
+
+}
